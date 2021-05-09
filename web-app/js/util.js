864a5d224d71594a715b8f1c83fcb980201689d9
dicksuckingfish <83911088+dicksuckingfish@users.noreply.github.com>
2021-05-09 15:13:48 +0200

    Initial commit

---
+++ web-app/js/util.js
@@ -0,0 +1,4 @@
+var API_KEY = 'Flight%20booker%201.2%20plus%20edition';
+var URL = 'http://127.0.0.1:41062/www/api/API.php?API_KEY='+API_KEY;
+
+var URL_STATUS = 'http://127.0.0.1:41062/www/cgi-bin/status_check?';
